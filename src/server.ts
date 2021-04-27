25b3e18e472c1bca32b2cd8f981ad4f56318be3e
Hilderal Térence <terencehilderal@gmail.com>
2021-04-27 19:49:24 +0200

    update :defining /api as initial route

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,17 @@
 import express, { Request, Response } from 'express';
 import path from 'path';
 require('dotenv').config();
-require('./database')
+require('./database');
+const usersRoutes = require('./components/users/users.routes.js');
 
 const PORT = process.env.PORT || 7000;
 
 const app = express();
-
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('frontend/build'));
 
-app.get('/api', (req: Request, res: Response) => {
-	res.send({ msg: 'Hello Master972' });
-});
+app.use('/api', usersRoutes);
 
 app.get('/*', (req: Request, res: Response) => {
 	res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
