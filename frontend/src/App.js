db35abd2ce9e78bf27392f5dad2944d48327eb32
Hilderal Térence <terencehilderal@gmail.com>
2021-05-05 13:33:40 +0200

    update : creating a handleAlert function and defining a usercontext to pass it throught the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Homepage from './components/Homepage';
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
+import Alerts from './components/Alert';
+export const UserContext = createContext();
 
 const App = () => {
-	const UserContext = createContext();
+	const [alert, setAlert] = useState(null);
+
+	const handleAlert = (variant, text) => {
+		setAlert({ variant, text });
+		setTimeout(() => {
+			setAlert(null);
+		}, 3000);
+	};
+
 	return (
 		<>
-			<UserContext.Provider value>
+			<UserContext.Provider value={{ handleAlert, alert }}>
 				<Router>
 					<Route exact path='/' component={Homepage} />
+					<Route exact path='/alert' component={Alerts} />
 				</Router>
 			</UserContext.Provider>
 		</>
