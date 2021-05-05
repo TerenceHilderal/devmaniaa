1be43975e9253a923e81e4ec6e6f4eb981fdef66
Hilderal Térence <terencehilderal@gmail.com>
2021-05-05 13:40:58 +0200

    update : adding unique mongoose validator to send error message when username or email is already in our db

--- dist/database/models/user.model.js
+++ dist/database/models/user.model.js
@@ -5,17 +5,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var mongoose_1 = __importDefault(require("mongoose"));
 var bcrypt_1 = __importDefault(require("bcrypt"));
+var mongoose_unique_validator_1 = __importDefault(require("mongoose-unique-validator"));
 var Schema = mongoose_1.default.Schema;
 var userSchema = new Schema({
-    email: { type: String, required: true, unique: true },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
     password: { type: String, required: true, unique: true },
-    username: { type: String, required: true, unique: true, maxlength: 30 },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        maxlength: 30,
+    },
     description: { type: String, required: false, maxlength: 200 },
     posts: { type: [String], required: false },
     avatar: { type: String, required: false },
     friends: { type: [String], required: false },
     isAdmin: { type: Boolean, default: 0 },
 });
+userSchema.plugin(mongoose_unique_validator_1.default, { message: 'already taken' });
 userSchema.statics.hashPassword = function (password) {
     return bcrypt_1.default.hash(password, 12);
 };
--- src/database/models/user.model.ts
+++ src/database/models/user.model.ts
@@ -1,11 +1,21 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
+import validator from 'mongoose-unique-validator';
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-	email: { type: String, required: true, unique: true },
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
 	password: { type: String, required: true, unique: true },
-	username: { type: String, required: true, unique: true, maxlength: 30 },
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+		maxlength: 30,
+	},
 	description: { type: String, required: false, maxlength: 200 },
 	posts: { type: [String], required: false },
 	avatar: { type: String, required: false },
@@ -13,6 +23,7 @@ const userSchema = new Schema({
 	isAdmin: { type: Boolean, default: 0 },
 });
 
+userSchema.plugin(validator, { message: 'already taken' });
 userSchema.statics.hashPassword = (password) => {
 	return bcrypt.hash(password, 12);
 };
