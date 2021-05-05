92fd9064001b6813331b38425f43ae6711d6a385
Hilderal Térence <terencehilderal@gmail.com>
2021-05-05 13:31:33 +0200

    update : mounting alert component and logic in signup component

--- frontend/src/components/Signup/index.js
+++ frontend/src/components/Signup/index.js
@@ -1,13 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import SignupForm from './SignupForm';
 import Buttons from '../Buttons';
 import PropTypes from 'prop-types';
+import Alerts from '../Alert';
+import { UserContext } from '../../App';
 
 export const Signup = () => {
 	const [show, setShow] = useState(false);
 	const handleClose = () => setShow(false);
 	const handleShow = () => setShow(true);
+	const { alert } = useContext(UserContext);
 
 	return (
 		<>
@@ -20,11 +23,12 @@ export const Signup = () => {
 
 			<Modal show={show} onHide={handleClose}>
 				<Modal.Header closeButton>
-					<Modal.Title>Welcome! Welcome </Modal.Title>
+					<Modal.Title>Welcome! Welcome </Modal.Title> <br />
 				</Modal.Header>
 				<Modal.Body>
 					<SignupForm />
 				</Modal.Body>
+				{alert && <Alerts variant={alert.variant} text={alert.text} />}
 			</Modal>
 		</>
 	);
