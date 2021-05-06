71035384b8ec180a737db9d7aa81f83558ae62f2
Hilderal Térence <terencehilderal@gmail.com>
2021-05-06 14:57:47 +0200

    adding login controller on login route

--- dist/components/users/users.routes.js
+++ dist/components/users/users.routes.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var usersCtrl = require('./users.controller');
+var auth = require('../../database/middleware/auth');
 var router = require('express').Router();
 router.post('/signup', usersCtrl.signup);
-router.post('/login');
+router.post('/login', usersCtrl.login);
 module.exports = router;
--- src/components/users/users.routes.ts
+++ src/components/users/users.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 const usersCtrl = require('./users.controller');
-
+const auth = require('../../database/middleware/auth');
 const router: Router = require('express').Router();
 
 router.post('/signup', usersCtrl.signup);
-router.post('/login');
+router.post('/login', usersCtrl.login);
 
 module.exports = router;
