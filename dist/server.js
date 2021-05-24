654804e1ca43b3a6b740a3d8eb8b0682b9d0d6b3
Hilderal Térence <terencehilderal@gmail.com>
2021-05-24 11:33:07 +0200

    update : adding multer middleware

--- dist/server.js
+++ dist/server.js
@@ -8,20 +8,24 @@ var path_1 = __importDefault(require("path"));
 require('dotenv').config();
 require('./database');
 var usersRoutes = require('./components/users/users.routes.js');
+var postsRoutes = require('./components/posts/posts.routes');
 var PORT = process.env.PORT || 7000;
 var app = express_1.default();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
-app.use(express_1.default.static('frontend/build'));
+// app.use(express.static('frontend/build'));
+app.use('/uploads', express_1.default.static(path_1.default.join(__dirname, 'uploads')));
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Origin', '*'); //l'origine qui a le droit d'accéder à notre api = tout le monde
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content, Accept, Content-Type, Authorization,*'); //on autorise certains headers
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // on autorise certaines méthod
     next();
 });
-app.use('/api/users', usersRoutes);
-app.get('/*', function (req, res) {
+app.get('*', function (req, res) {
     res.sendFile(path_1.default.join(__dirname, '../frontend/build', 'index.html'));
 });
+app.use('/api/users/', usersRoutes);
+app.use('/api/posts/', postsRoutes);
 app.listen(7000, function () {
     console.log("Server running on port " + PORT);
 });
--- src/server.ts
+++ src/server.ts
@@ -3,28 +3,33 @@ import path from 'path';
 require('dotenv').config();
 require('./database');
 const usersRoutes = require('./components/users/users.routes.js');
+const postsRoutes = require('./components/posts/posts.routes');
 
 const PORT = process.env.PORT || 7000;
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('frontend/build'));
+// app.use(express.static('frontend/build'));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-app.use(function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-	res.header(
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*'); //l'origine qui a le droit d'accéder à notre api = tout le monde
+	res.setHeader(
 		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept',
-	);
+		'Origin, X-Requested-with, Content, Accept, Content-Type, Authorization,*',
+	); //on autorise certains headers
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+	); // on autorise certaines méthod
 	next();
 });
-
-app.use('/api/users', usersRoutes);
-
-app.get('/*', (req: Request, res: Response) => {
+app.get('*', (req: Request, res: Response) => {
 	res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
+app.use('/api/users/', usersRoutes);
+app.use('/api/posts/', postsRoutes);
 
 app.listen(7000, () => {
 	console.log(`Server running on port ${PORT}`);
