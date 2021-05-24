f439e857ff736d3ab58d066962dfc2c99d079aea
Hilderal Térence <terencehilderal@gmail.com>
2021-05-24 11:37:24 +0200

    deleting auth requirement in users routes

--- dist/components/users/users.routes.js
+++ dist/components/users/users.routes.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var usersCtrl = require('./users.controller');
-var auth = require('../../database/middleware/auth');
 var router = require('express').Router();
 router.post('/signup', usersCtrl.signup);
 router.post('/login', usersCtrl.login);
--- src/components/users/users.routes.ts
+++ src/components/users/users.routes.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 const usersCtrl = require('./users.controller');
-const auth = require('../../database/middleware/auth');
 const router: Router = require('express').Router();
 
 router.post('/signup', usersCtrl.signup);
